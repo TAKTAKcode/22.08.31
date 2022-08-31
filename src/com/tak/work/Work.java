7f50de2e005963e1fdf16a5923f82f49cf4b0d9d
tak <wontaek22@gmail.com>
2022-08-31 21:06:40 +0900

    22.08.31
    Work

---
+++ src/com/tak/work/Work.java
@@ -0,0 +1,10 @@
+package com.tak.work;
+
+public class Work {
+
+    public static void main(String[] args) {
+
+        System.out.println("회사에서 교육 3일차. 요즘은 스프링기반으로 게시판을 만들고 있다. 그걸 Commit하고 싶지만 아무래도 사내 교육자료라 안될 것 같다.");
+    }
+
+}
